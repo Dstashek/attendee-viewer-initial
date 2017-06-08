babfc23a9d525b8749c8a78af99826e63bd1546e
AMS\daniel.stashek <daniel.stashek@cgi.com>
2017-06-08 11:24:13 -0600

    added in security detection

--- src/main/java/io/rscale/training/attendeeviewer/AttendeeViewerApplication.java
+++ src/main/java/io/rscale/training/attendeeviewer/AttendeeViewerApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.boot.autoconfigure.security.oauth2.client;
 
 
 @SpringBootApplication
@@ -13,10 +14,18 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 @EnableDiscoveryClient
 @EnableEurekaClient
 @EnableCircuitBreaker
-public class AttendeeViewerApplication {
+@EnableOAuth2Sso
+public class AttendeeViewerApplication extends WebSecurityConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AttendeeViewerApplication.class, args);
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+	    http.authorizeRequests()
+	        .antMatchers("/login").permitAll()
+	        .anyRequest().authenticated();
 	}
 	
 
